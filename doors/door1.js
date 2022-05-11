be097480290edcc4032b039124976251e5073826
Arthur Corona <coronaggp@gmail.com>
2022-05-11 19:41:32 -0300

    door1 ok

--- doors/door1.js
+++ doors/door1.js
@@ -1,7 +1,10 @@
 function discordo() {
     document.getElementById("discordo").style.display = "block"
+    document.getElementById("title").style.display = "none"
 }
 
 function concordo() {
     document.getElementById("concordo").style.display = "block"
+    document.getElementById("title").style.display = "none"
 }
+
